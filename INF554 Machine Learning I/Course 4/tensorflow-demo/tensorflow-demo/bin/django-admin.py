8bbd5b07623b001e0902fcd158f9cd894aefd484
LotfiKobrosly <kobroslotfi@gmail.com>
2018-12-27 16:48:34 +0100

    Training on neural networks with TensorFlow

---
+++ INF554 Machine Learning I/Course 4/tensorflow-demo/tensorflow-demo/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/lotfi/Documents/Polytechnique/Academic_work/INF554 Machine Learning I/Course 4/tensorflow-demo/tensorflow-demo/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
