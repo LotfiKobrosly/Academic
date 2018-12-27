b78edf3e7dc0a55bd4c4dea0ca4e24843b9dd97d
LotfiKobrosly <kobroslotfi@gmail.com>
2018-12-27 16:21:35 +0100

    Finalizing

--- INF554 Machine Learning I/Course 4/testing_tf.py
+++ INF554 Machine Learning I/Course 4/testing_tf.py
@@ -1,12 +1,10 @@
-import tensorflow as tf 
+import tensorflow as tf
 
 x1 = tf.constant([1,2,3,4])
 x2 = tf.constant([5,6,7,8])
-
 result = tf.multiply(x1,x2)
 
 sess = tf.Session()
-
 res = sess.run(result)
 
 sess.close()
